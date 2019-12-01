88ed3dfc24ad0193e9344e962f273fa6e0aa5279
cezarpm <cezar_pm@hotmail.com>
2019-12-01 18:02:37 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+
+import Header from "./components/Header/Header";
 import "./App.css";
 
 const App = () => {
-  return <h1>Isso ai</h1>;
+  return <Header />;
 };
 
 export default App;
