5dd46401c22a97c32001754a783f13b47f25a94a
cezarpm <cezar_pm@hotmail.com>
2019-12-01 18:56:46 -0300

    axios and getRequest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 import Header from "./components/Header/Header";
+import CardList from "./components/CardList/CardList";
 import "./App.css";
 
 const App = () => {
-  return <Header />;
+  const [loadedCoins, setLoadedCoins] = useState([]);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      const response = await axios
+        .get(`https://api.coinlore.com/api/ticker/?id=90`)
+        .then(data => data.data[0])
+        .catch(error => error);
+
+      setLoadedCoins(response);
+    };
+
+    fetchCoins();
+  });
+
+  return (
+    <>
+      <Header />
+      <main>
+        <CardList coins={loadedCoins} />
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Card from "../Card/Card.js";
+
+function CardList() {
+  return (
+    <ul>
+      <Card />
+    </ul>
+  );
+}
+
+export default CardList;
